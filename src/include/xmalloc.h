d98cb8a889cc50c713991409abfd31ce3c61329c
Zeromemer <zeromemer@users.noreply.github.com>
2022-02-17 14:41:47 +0200

    made the xmalloc functions macros the internal functions now take file and line for debuging purposes also when they fail, the internal functions raise a SIGTRAP

--- src/include/xmalloc.h
+++ src/include/xmalloc.h
@@ -2,9 +2,15 @@
 #define XMALLOC_H
 #include <stddef.h>
 
-void *xmalloc(size_t size);
-void *xcalloc(size_t nmemb, size_t size);
-void *xrealloc(void *ptr, size_t size);
-void *xreallocarray(void *ptr, size_t nmemb, size_t size);
+void *xmalloc_inter(size_t size, char *file, int line);
+void *xcalloc_inter(size_t nmemb, size_t size, char *file, int line);
+void *xrealloc_inter(void *ptr, size_t size, char *file, int line);
+void *xreallocarray_inter(void *ptr, size_t nmemb, size_t size, char *file, int line);
+
+
+#define xmalloc(size) xmalloc_inter(size, __FILE__, __LINE__)
+#define xcalloc(nmemb, size) xcalloc_inter(nmemb, size, __FILE__, __LINE__)
+#define xrealloc(ptr, size) xrealloc_inter(ptr, size, __FILE__, __LINE__)
+#define xreallocarray(ptr, nmemb, size) xreallocarray_inter(ptr, nmemb, size, __FILE__, __LINE__)
 
 #endif
--- src/xmalloc.c
+++ src/xmalloc.c
@@ -1,40 +1,45 @@
 #include "include/xmalloc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
 // TODO: make the xmalloc functions thread safe using semaphores
 
-void *xmalloc(size_t size) {
+void *xmalloc_inter(size_t size, char *file, int line) {
 	void *result = malloc(size);
 	if (!result) {
-		fprintf(stderr, "ERROR: malloc(%ld) returned NULL\n", size);
+		fprintf(stderr, "%s:%d: malloc(%ld) returned NULL\n", file, line, size);
+		raise(SIGTRAP);
 		exit(1);
 	}
 	return result;
 }
 
-void *xcalloc(size_t nmemb, size_t size) {
+void *xcalloc_inter(size_t nmemb, size_t size, char *file, int line) {
 	void *result = calloc(nmemb, size);
 	if (!result) {
-		fprintf(stderr, "ERROR: calloc(%ld ,%ld) returned NULL\n", nmemb, size);
+		fprintf(stderr, "%s:%d: calloc(%ld ,%ld) returned NULL\n", file, line, nmemb, size);
+		raise(SIGTRAP);
 		exit(1);
 	}
 	return result;	
 }
 
-void *xrealloc(void *ptr, size_t size) {
+void *xrealloc_inter(void *ptr, size_t size, char *file, int line) {
 	void *result = realloc(ptr, size);
 	if (!result) {
-		fprintf(stderr, "ERROR: realloc(%p, %ld) returned NULL\n", ptr, size);
+		fprintf(stderr, "%s:%d: realloc(%p, %ld) returned NULL\n", file, line, ptr, size);
+		raise(SIGTRAP);
 		exit(1);
 	}
 	return result;
 }
 
-void *xreallocarray(void *ptr, size_t nmemb, size_t size) {
+void *xreallocarray_inter(void *ptr, size_t nmemb, size_t size, char *file, int line) {
 	void *result = reallocarray(ptr, nmemb, size);
 	if (!result) {
-		fprintf(stderr, "ERROR: reallocarray(%p, %ld, %ld) returned NULL\n", ptr, nmemb, size);
+		fprintf(stderr, "%s:%d: reallocarray(%p, %ld, %ld) returned NULL\n", file, line, ptr, nmemb, size);
+		raise(SIGTRAP);
 		exit(1);
 	}
 	return result;
