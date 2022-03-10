934c3ab15578dcd9e74af1b59ffead4f8529d0a8
Zeromemer <zeromemer@users.noreply.github.com>
2022-03-10 23:55:24 +0200

    made port an optional command line argument

--- src/main.c
+++ src/main.c
@@ -13,17 +13,22 @@
 #include "include/xmalloc.h"
 #include "include/req_handl.h"
 
-#define PORT 42069
-
 int main(int argc, char **argv) {
+	char *port_s = argv[1];
+	int port = port_s ? atoi(port_s) : 42069;
+	if (port > 65535) {
+		fprintf(stderr, "Could not bind socket: Port number out of range\n"); // if libc isn't going to check the port number, I will
+		return 1;
+	}
+
 	struct sigaction sig_conf = {{SIG_IGN}};
 	sigaction(SIGPIPE, &sig_conf, NULL);
 	
-	int sockfd = create_bound_socket(PORT);
+	int sockfd = create_bound_socket(port);
 
 	socket_listen(sockfd, 5);
 
-	printf("Listening on port %d\n", PORT);
+	printf("Listening on port %d\n", port);
 	for (;;) {
 		conn_t *conn = xcalloc(1, sizeof(conn_t));
 		int status = await_connection(sockfd, conn);
